9c33db17f4bdec53ed1e58851afd6ff05493d65e
Julio <cebrerojulio10@gmail.com>
2026-02-20 22:15:08 -0600

    Finished Java Doc style comments for Rule 1

--- src/Rule1/Dog.java
+++ src/Rule1/Dog.java
@@ -1,9 +1,29 @@
+/**
+ * @author Julio Cebrero
+ * 
+ * Rule 05-OBJ11-J: Be wary of letting constructors
+ * throw exceptions
+ * 
+ * Represents a Dog object that helps demonsrate the rule by
+ * declaring the class final to prevent finalizer attacks when
+ * an Exception is thrown by the constructor.
+ */
+
 package src.Rule1;
 
 public final class Dog {
+    /** The name of the dog */
     private String name;
+
+    /** The age of the dog in years */
     private int age;
 
+    /**
+     * Contructs a fully intialized Dog object
+     * @param name the name of the dog, cannot be null
+     * @param age the age of the dog
+     * @throws IllegalArgumentException
+     */
     public Dog(String name,int age){
         if(name==null){
             throw new IllegalArgumentException("Name cannot be null!");
@@ -12,10 +32,16 @@ public final class Dog {
         this.age=age;
     }
 
+    /**
+     * Prints the sound a dog makes.
+     */
     public void dogNoise(){
         System.out.println("bark");
     }
 
+    /**
+     * Displays the dog's information.
+     */
     public void getInfo(){
         System.out.println("Dog Information:\nName: "+this.name+"\nAge: "+this.age+" years old");
     }
--- src/Rule1/Main.java
+++ src/Rule1/Main.java
@@ -5,16 +5,26 @@
  * throw exceptions
  * 
  * Helps demonstarte the compliant solution using
- * a finnal class to prvent finalizer attacks
+ * a final class to prevent finalizer attacks
  */
 package src.Rule1;
 
 public class Main {
+
+    /**
+     * Entry point of the program. Demonsrates a
+     * fully initialized Dog object and a failed
+     * partial initialization attempt.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
+        //Valid fully initialized Dog
         Dog first=new Dog("Bob",5);
         first.dogNoise();
         first.getInfo();
 
+        //Partially initialized - will throw exception
         try{
             Dog second=new Dog(null,4);
         } catch(IllegalArgumentException e){
